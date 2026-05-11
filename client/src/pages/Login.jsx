abb28d99425f9e0f1df656a1168abe9d13979516
diptanil98 <diptanilsarkar49@gmail.com>
2026-05-11 11:34:50 +0530

    first commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -111,7 +111,7 @@ const Login = () => {
               <div className="w-full border-t border-gray-200"></div>
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="px-3 bg-white text-gray-400 text-xs">New to TaskFlow?</span>
+              <span className="px-3 bg-white text-gray-400 text-xs">New to FlowDesk?</span>
             </div>
           </div>
 
